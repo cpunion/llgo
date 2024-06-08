ff9ee38ddd38108f257762daa51810d19c3c6671
Li Jie <cpunion@gmail.com>
2024-06-08 11:03:35 +0800

    ci: comment test result on PR

--- .github/workflows/test_demo.sh
+++ .github/workflows/test_demo.sh
@@ -14,16 +14,19 @@ for d in ./_demo/* ./_pydemo/*; do
     if [ $? -ne 0 ]; then
       echo "FAIL"
       failed=$((failed+1))
-      failed_cases="$failed_cases\n$d"
+      failed_cases="$failed_cases\n:x: $d"
     else
       echo "PASS"
     fi
   fi
 done
-
 echo "=== Done"
 echo "$((total-failed))/$total tests passed"
+
 if [ "$failed" -ne 0 ]; then
-  echo -e "Failed cases:$failed_cases"
+  echo ":bangbang: Failed demo cases:" | tee -a result.txt
+  echo -e "$failed_cases" | tee -a result.txt
   exit 1
+else
+  echo ":white_check_mark: All demo tests passed" | tee -a result.txt
 fi
