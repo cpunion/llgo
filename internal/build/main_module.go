6c7c3a79e576f6acc802f813811ee82f5e287c1f
Li Jie <cpunion@gmail.com>
2026-07-28 02:33:38 +0800

    runtime/wasm: add WASI single-worker scheduler

--- internal/build/main_module.go
+++ internal/build/main_module.go
@@ -121,10 +121,16 @@ func genMainModule(ctx *context, rtPkgPath string, pkg *packages.Package, cfg *g
 		return mainAPkg
 	}
 
+	var wasmRunMain llssa.Function
+	if ctx.crossCompile.WasmPostLink.Asyncify {
+		defineWasmMainTask(mainPkg, mainInit, mainMain)
+		wasmRunMain = declareNoArgFunc(mainPkg, rtPkgPath+".RunWasmMain")
+	}
 	entryFn := defineEntryFunction(ctx, mainPkg, argcVar, argvVar, argvValueType, entryFunctions{
 		runtimeStub: runtimeStub,
 		mainInit:    mainInit,
 		mainMain:    mainMain,
+		wasmRunMain: wasmRunMain,
 		pyInit:      pyInit,
 		pyFinalize:  pyFinalize,
 		rtInit:      rtInit,
@@ -219,6 +225,7 @@ type entryFunctions struct {
 	runtimeStub llssa.Function
 	mainInit    llssa.Function
 	mainMain    llssa.Function
+	wasmRunMain llssa.Function
 	pyInit      llssa.Function
 	pyFinalize  llssa.Function
 	rtInit      llssa.Function
@@ -261,13 +268,32 @@ func defineEntryFunction(ctx *context, pkg llssa.Package, argcVar, argvVar llssa
 		b.Call(fns.abiInit.Expr)
 	}
 	b.Call(fns.runtimeStub.Expr)
-	b.Call(fns.mainInit.Expr)
-	b.Call(fns.mainMain.Expr)
+	if fns.wasmRunMain != nil {
+		b.Call(fns.wasmRunMain.Expr)
+	} else {
+		b.Call(fns.mainInit.Expr)
+		b.Call(fns.mainMain.Expr)
+	}
 	if fns.pyFinalize != nil {
 		b.Call(fns.pyFinalize.Expr)
 	}
 	b.Return(prog.IntVal(0, prog.Int32()))
 	return fn
+}
+
+func defineWasmMainTask(pkg llssa.Package, mainInit, mainMain llssa.Function) {
+	prog := pkg.Prog
+	sig := newSignature(
+		[]types.Type{types.Typ[types.UnsafePointer]},
+		[]types.Type{types.Typ[types.UnsafePointer]},
+	)
+	fn := pkg.NewFunc("__llgo_wasm_main", sig, llssa.InC)
+	fnVal := pkg.Module().NamedFunction("__llgo_wasm_main")
+	fnVal.SetVisibility(llvm.HiddenVisibility)
+	b := fn.MakeBody(1)
+	b.Call(mainInit.Expr)
+	b.Call(mainMain.Expr)
+	b.Return(prog.Nil(prog.VoidPtr()))
 }
 
 func defineStart(pkg llssa.Package, entry llssa.Function, argvType llssa.Type) {
