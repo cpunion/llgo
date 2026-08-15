a00313e13ab2ed72ac0693d12db3a2a94c1d9f32
Li Jie <cpunion@gmail.com>
2026-08-15 23:34:58 +0800

    target: emit COMDAT for shared COFF definitions

---
+++ ssa/coff_comdat_test.go
@@ -0,0 +1,51 @@
+//go:build !llgo
+
+package ssa
+
+import (
+	"go/types"
+	"strings"
+	"testing"
+
+	"github.com/xgo-dev/llvm"
+)
+
+func TestWindowsODRDefinitionsUseCOMDAT(t *testing.T) {
+	prog := NewProgram(&Target{GOOS: "windows", GOARCH: "amd64"})
+	defer prog.Dispose()
+	pkg := prog.NewPackage("example.com/p", "example.com/p")
+
+	global := pkg.NewVar("shared.global", types.NewPointer(types.Typ[types.Int]), InGo)
+	global.Init(prog.IntVal(0, prog.Int()))
+	pkg.setODRLinkage(global.impl, llvm.WeakODRLinkage)
+	fn := pkg.NewFuncEx("shared.func", NoArgsNoRet, InGo, false, true)
+	body := fn.MakeBody(1)
+	body.Return()
+	body.EndBuild()
+
+	ir := pkg.String()
+	for _, want := range []string{
+		`$shared.global = comdat any`,
+		`$shared.func = comdat any`,
+		`@shared.global = weak_odr global i64 0, comdat`,
+		`define linkonce void @shared.func() #0 comdat`,
+	} {
+		if !strings.Contains(ir, want) {
+			t.Fatalf("Windows ODR IR does not contain %q:\n%s", want, ir)
+		}
+	}
+}
+
+func TestUnixODRDefinitionsDoNotGainCOMDAT(t *testing.T) {
+	prog := NewProgram(&Target{GOOS: "linux", GOARCH: "amd64"})
+	defer prog.Dispose()
+	pkg := prog.NewPackage("example.com/p", "example.com/p")
+
+	global := pkg.NewVar("shared.global", types.NewPointer(types.Typ[types.Int]), InGo)
+	global.Init(prog.IntVal(0, prog.Int()))
+	pkg.setODRLinkage(global.impl, llvm.WeakODRLinkage)
+
+	if ir := pkg.String(); strings.Contains(ir, "comdat") {
+		t.Fatalf("non-Windows ODR IR unexpectedly contains COMDAT:\n%s", ir)
+	}
+}
