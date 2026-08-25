0561746e57bf5a8092b40d27dd0bf2f57c5d289c
Li Jie <cpunion@gmail.com>
2026-08-26 07:06:36 +0800

    ssa: tighten inline array equality lowering

--- cl/compile.go
+++ cl/compile.go
@@ -2325,8 +2325,13 @@ func (p *context) compileInstrOrValue(b llssa.Builder, iv instrOrValue, asValue 
 		}
 		x := p.compileValueAs(b, v.X, v.Y.Type())
 		y := p.compileValueAs(b, v.Y, v.X.Type())
-		if _, ok := v.X.Type().Underlying().(*types.Array); ok && (v.Op == token.EQL || v.Op == token.NEQ) {
-			ret = b.ArrayBinOp(v.Op, x, y, p.arrayCompareAddr(b, v.X), p.arrayCompareAddr(b, v.Y))
+		if typ, ok := v.X.Type().Underlying().(*types.Array); ok && (v.Op == token.EQL || v.Op == token.NEQ) {
+			xaddr, yaddr := llssa.Nil, llssa.Nil
+			if !llssa.CanInlineArrayEqual(typ) {
+				xaddr = p.arrayCompareAddr(b, v.X)
+				yaddr = p.arrayCompareAddr(b, v.Y)
+			}
+			ret = b.ArrayBinOp(v.Op, x, y, xaddr, yaddr)
 		} else if physicalPlanned && physicalInstruction.recipe == coroPhysicalInstructionIntegerDivideByZeroGuard {
 			observePhysical(coroPhysicalInstructionIntegerDivideByZeroGuard)
 			zero := p.prog.Zero(y.Type)
