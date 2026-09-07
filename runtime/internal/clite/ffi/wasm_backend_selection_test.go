bfc7ddd6d368427605d9f140446c614a2fc25481
Li Jie <cpunion@gmail.com>
2026-09-07 20:59:16 +0800

    runtime/wasm: enable reflection FFI for JavaScript profiles

---
+++ runtime/internal/clite/ffi/wasm_backend_selection_test.go
@@ -0,0 +1,31 @@
+package ffi
+
+import (
+	"go/build"
+	"slices"
+	"testing"
+)
+
+func TestWasmBackendSelection(t *testing.T) {
+	for _, test := range []struct {
+		name string
+		goos string
+		want string
+	}{
+		{name: "Go-compatible JavaScript", goos: "js", want: "link_flags_wasm_emscripten.go"},
+		{name: "Go-compatible WASI", goos: "wasip1", want: "link_flags_wasm.go"},
+	} {
+		t.Run(test.name, func(t *testing.T) {
+			ctx := build.Default
+			ctx.GOOS = test.goos
+			ctx.GOARCH = "wasm"
+			pkg, err := ctx.ImportDir(".", 0)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !slices.Contains(pkg.GoFiles, test.want) {
+				t.Fatalf("GoFiles = %v, want %s", pkg.GoFiles, test.want)
+			}
+		})
+	}
+}
