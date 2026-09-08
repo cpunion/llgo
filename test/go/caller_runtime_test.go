8c4cb1570023f79a378731fd99bcffab5d67afbb
Li Jie <cpunion@gmail.com>
2026-09-09 03:54:09 +0800

    test: isolate panic traceback format from crash diagnostics

--- test/go/caller_runtime_test.go
+++ test/go/caller_runtime_test.go
@@ -59,7 +59,8 @@ func callerPanicCaller() {
 
 func testCallerPanicTraceback(t *testing.T) {
 	cmd := exec.Command(os.Args[0], "-test.run=^$")
-	cmd.Env = append(os.Environ(), callerPanicChild+"=1")
+	// Assert the default traceback format independently of parent crash diagnostics.
+	cmd.Env = append(os.Environ(), callerPanicChild+"=1", "GOTRACEBACK=single")
 	output, err := cmd.CombinedOutput()
 	if err == nil {
 		t.Fatalf("panic child unexpectedly succeeded:\n%s", output)
