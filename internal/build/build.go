d595373106de0b14e03848a647a61c7062547666
Li Jie <cpunion@gmail.com>
2026-09-09 08:54:13 +0800

    perf(wasm): lower adjacent aggregate copies without scalar expansion

--- internal/build/build.go
+++ internal/build/build.go
@@ -2960,6 +2960,7 @@ func compilePackageModule(ctx *context, aPkg *aPackage, externs []string, verbos
 		}
 	}
 	coalesceWasmPanicLocations(ctx.buildConf.Goarch, ret.Module())
+	lowerWasmAggregateCopies(ctx.buildConf.Goarch, ctx.prog.TargetData(), ret.Module())
 	applySizeOptimizationAttributes(ret.Module(), ctx.buildConf.OptLevel)
 	printCmds := ctx.shouldPrintCommands(verbose)
 	if ctx.mode != ModeGen {
