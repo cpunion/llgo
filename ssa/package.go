1cd076daa965e073e0e9259f070b2a05c48596c5
Li Jie <cpunion@gmail.com>
2026-09-11 20:43:11 +0800

    feat(wasm): support reflection calls in WASI

--- ssa/package.go
+++ ssa/package.go
@@ -968,6 +968,8 @@ type aPackage struct {
 	llvmUsedValues []llvm.Value
 
 	abiTypeFakeUseCache map[llvm.Value][]llvm.Value
+
+	wasmReflectBridges map[string]wasmReflectBridgePair
 }
 
 type none struct{}
